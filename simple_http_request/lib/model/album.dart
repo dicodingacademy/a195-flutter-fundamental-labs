8a62f0bebf61ed89730badebf3aee7eabf23fbd9
rrifafauzikomara <rifafauzi6@gmail.com>
2020-09-21 18:18:31 +0700

    grouping file

---
+++ simple_http_request/lib/model/album.dart
@@ -0,0 +1,15 @@
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({this.userId, this.id, this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
