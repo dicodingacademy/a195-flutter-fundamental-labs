4d7476bbf7c7349b1833d04e0cf5796ebc880688
Widyarso Joko Purnomo <widyjp10@gmail.com>
2021-11-30 16:37:46 +0700

    add learning path (sliver) sample app

---
+++ learning_paths/android/app/src/main/kotlin/com/dicoding/learning_paths/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dicoding.learning_paths
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ learning_paths/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:learning_paths/learning_path_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LearningPathPage(),
+    );
+  }
+}
