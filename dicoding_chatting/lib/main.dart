e0995105b2106723554f37046c80828bb8ff8d60
Achmad Ilham <achmadilham07@gmail.com>
2022-06-17 09:29:06 +0700

    integration flutter-firebase

--- dicoding_chatting/lib/main.dart
+++ dicoding_chatting/lib/main.dart
@@ -2,8 +2,15 @@ import 'package:dicoding_chatting/pages/chat_page.dart';
 import 'package:dicoding_chatting/pages/login_page.dart';
 import 'package:dicoding_chatting/pages/register_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
