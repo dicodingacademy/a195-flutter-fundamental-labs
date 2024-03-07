f61eeca42e733dca0ae1dbcb134f02b0cf5d1a7b
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2024-03-07 13:50:16 +0700

    add navigation project

---
+++ 02-navigation/02-final/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:navigation_app/another_screen.dart';
+import 'package:navigation_app/first_screen.dart';
+import 'package:navigation_app/replacement_screen.dart';
+import 'package:navigation_app/return_data_screen.dart';
+import 'package:navigation_app/second_screen.dart';
+import 'package:navigation_app/second_screen_with_data.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const FirstScreen(),
+        '/second': (context) => const SecondScreen(),
+        '/second-with-data': (context) => SecondScreenWithData(
+              data: ModalRoute.of(context)?.settings.arguments as String,
+            ),
+        '/return-data': (context) => const ReturnDataScreen(),
+        '/replacement': (context) => const ReplacementScreen(),
+        '/another': (context) => const AnotherScreen(),
+      },
+    );
+  }
+}
