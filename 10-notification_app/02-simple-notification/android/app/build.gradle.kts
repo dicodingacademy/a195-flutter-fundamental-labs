595ebad898657e9f5fa7941ec1393b25be36ba6c
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2025-03-28 10:10:04 +0700

    finalize native project

--- 10-notification_app/02-simple-notification/android/app/build.gradle.kts
+++ 10-notification_app/02-simple-notification/android/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
